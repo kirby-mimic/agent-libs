d36a04ed4b07311347c7a96d515f3254297fa9b0
Iacopo Rozzo <iacopo@sysdig.com>
2022-12-10 10:06:45 +0100

    Fix build

--- userspace/libsinsp/container_engine/cri.cpp
+++ userspace/libsinsp/container_engine/cri.cpp
@@ -315,7 +315,8 @@ bool cri::resolve(sinsp_threadinfo *tinfo, bool query_os_for_missing_info)
 		if(!m_async_source)
 		{
 			uint64_t max_wait_ms = 20000;
-			m_async_source = std::unique_ptr<cri_async_source>(cache, m_cri.get(), max_wait_ms);
+            auto async_source = new cri_async_source(cache, m_cri.get(), max_wait_ms);
+            m_async_source = std::unique_ptr<cri_async_source>(async_source);
 			g_logger.format(sinsp_logger::SEV_DEBUG,
 					"cri_async (%s): Created async source with max wait %lu",
 					container_id.c_str(),
