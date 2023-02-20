69260809eaa0fa08d7e734b4ddf10f0f386810c2
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2023-02-20 16:04:09 +0100

    new(scap,gvisor): configurable epoll_wait timeout
    gvisor's next() calls epoll_wait to wait for the next event,
    with an infinite timeout. Other engines don't block in next()
    forever, so to remain compatible (and allow other things in
    the main thread except for a scap_next() loop), make the timeout
    configurable (still defaults to infinite at the sinsp layer).
    
    Signed-off-by: Grzegorz Nosek <grzegorz.nosek@sysdig.com>

--- userspace/libscap/engine/gvisor/gvisor.h
+++ userspace/libscap/engine/gvisor/gvisor.h
@@ -132,7 +132,7 @@ class engine {
 public:
     engine(char *lasterr);
     ~engine();
-    int32_t init(std::string config_path, std::string root_path, bool no_events);
+    int32_t init(std::string config_path, std::string root_path, bool no_events, int epoll_timeout);
     int32_t close();
 
     int32_t start_capture();
@@ -151,6 +151,7 @@ private:
     char *m_lasterr;
     int m_listenfd = 0;
     int m_epollfd = 0;
+    int m_epoll_timeout = -1;
     bool m_capture_started = false;
     bool m_no_events = false;
 
